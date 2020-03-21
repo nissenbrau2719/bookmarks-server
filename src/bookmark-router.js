04faef8e7089518d533b99afb757e01585700bbf
Paul Nissen <nissenbrau@gmail.com>
2020-03-21 16:18:29 -0600

    wrote GET routes for /bookmark and /bookmark/:id and wrote POST route for /bookmark. Still need to validate url is formatted properly

--- src/bookmark-router.js
+++ src/bookmark-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { v4: uuid } = require('uuid');
 const logger = require('./logger');
+const { PORT } = require('./config');
 
 const bookmarks = [{
   title: "google",
@@ -10,10 +11,75 @@ const bookmarks = [{
   id: 1
 }]
 
-bookmarkRouter = express.Router()
+const bookmarkRouter = express.Router();
+const bodyParser = express.json();
 
 //build routes to GET and POST at /bookmarks
+bookmarkRouter
+  .route('/bookmarks')
+  .get((req, res) => {
+    res.json(bookmarks)
+  })
+  .post(bodyParser, (req, res) => {
+    const { title, url, description="", rating=1 } = req.body;
+
+    // validate request body
+    if (!title) {
+      logger.error('Title is required');
+      return res 
+        .status(400)
+        .send('Invalid data');
+    }
+
+    if (!url) {
+      logger.error('URL is required');
+      return res 
+        .status(400)
+        .send('Invalid data');
+    }
+
+    // validate that URL is properly formatted
+      // code here
+
+    const id = uuid();
+
+    const bookmark = {
+      id,
+      title,
+      url,
+      description,
+      rating
+    };
+
+    bookmarks.push(bookmark);
+
+    logger.info(`Bookmark with id ${id} created`);
+
+    res
+      .status(201)
+      .location(`http//localhost:${PORT}/bookmarks/${id}`)
+      .json(bookmark);
+
+  })
 
 //build routes to GET and DELETE at /bookmarks/:id
+bookmarkRouter
+  .route('/bookmarks/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    const bookmark = bookmarks.find(bm => bm.id == id);
+
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Bookmark not found');
+    }
+
+    res.json(bookmark);
+  })
+  .delete((req, res) => {
+
+  })
 
 module.exports = bookmarkRouter;
